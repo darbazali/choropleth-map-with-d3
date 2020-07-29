7ffc0715de4e3d34c458cd3bab9b0f65f8ef1f42
Darbaz Ali <darbaz.a.h@gmail.com>
2020-07-29 14:30:38 +0300

    define colorScale

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import * as d3 from "d3";
-import {feature} from 'topojson';
-
+import { feature } from "topojson";
 
 const container = d3.select("#container");
 
@@ -16,9 +15,10 @@ container
     "Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)"
   );
 
-
-const US_EDUCATION_DATA = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json";
-const US_COUNTY_DATA = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
+const US_EDUCATION_DATA =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json";
+const US_COUNTY_DATA =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
 
 let educationData;
 let countyData;
@@ -28,73 +28,63 @@ const width = 800,
 
 const canvas = container
   .append("svg")
-  .attr('width', width)
-  .attr('height', height)
+  .attr("width", width)
+  .attr("height", height);
+
+const legendData = [3, 12, 21, 30, 39, 48, 57, 66];
+const colorScale = d3
+  .scaleThreshold()
+  .domain(legendData)
+  .range(d3.schemeBlues[8])
+
+
 
 
 const drawMap = () => {
-    canvas
-      .selectAll('path')
-      .data(countyData)
-      .enter()
-      .append('path')
-      .attr('d', d3.geoPath())
-      .attr('class', 'county')
+  canvas
+    .selectAll("path")
+    .data(countyData)
+    .enter()
+    .append("path")
+    .attr("d", d3.geoPath())
+    .attr("class", "county")
 
-      .attr('fill', d => {
-        let id = d.id;
-        let county = educationData.find( item => {
-          return item.fips === id;
-        })
-        let percentage = county.bachelorsOrHigher;
+    .attr("fill", (d) => {
+      let id = d.id;
+      let county = educationData.find((item) => {
+        return item.fips === id;
+      });
+      let percentage = county.bachelorsOrHigher;
+      return colorScale(percentage)
+    }) // fill
 
-        if ( percentage <= 15 ) {
-          return 'tomato';
-        } else if ( percentage <= 30 ) {
-          return 'orange';
-        } else if ( percentage <= 45 ) {
-          return 'lightgreen';
-        } else {
-          return 'limegreen'
-        }
+    .attr("data-fips", (d) => d.id) // id
+    .attr("data-education", (d) => {
+      let id = d.id;
+      let county = educationData.find((item) => {
+        return item.fips === id;
+      });
+      let percentage = county.bachelorsOrHigher;
+      return percentage;
+    });
+};
 
-
-      }) // fill
-
-      .attr('data-fips', d => d.id) // id
-      .attr('data-education', d => {
-        let id = d.id;
-        let county = educationData.find( item => {
-          return item.fips === id;
-        })
-        let percentage = county.bachelorsOrHigher;
-        return percentage
-      })
-} 
-
-
-d3
-.json( US_COUNTY_DATA )
-.then( (data, error) => {
-  if ( error ) { 
-    throw error
+d3.json(US_COUNTY_DATA).then((data, error) => {
+  if (error) {
+    throw error;
   } else {
-    countyData = feature( data, data.objects.counties).features
+    countyData = feature(data, data.objects.counties).features;
     // console.log(countyData);
 
     // affter the first data srouce is loaded, then load the second one
-    d3
-      .json( US_EDUCATION_DATA )
-      .then( (data, error) => {
-        if ( error ) {
-          throw error;
-        } else {
-          educationData = data;
-          drawMap()
-          // console.log(data);
-        }
-      })
+    d3.json(US_EDUCATION_DATA).then((data, error) => {
+      if (error) {
+        throw error;
+      } else {
+        educationData = data;
+        drawMap();
+        // console.log(data);
+      }
+    });
   }
-  
-})
-
+});
