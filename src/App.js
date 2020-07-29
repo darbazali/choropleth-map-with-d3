a2d18e6665c71619f1417a28a7d615b4fa1868a0
Darbaz Ali <darbaz.a.h@gmail.com>
2020-07-29 09:43:55 +0300

    create title and description

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
+import * as d3 from 'd3'
+
+const container = d3.select('#container');
+
+container
+  .append('h2')
+  .attr('id', 'title')
+  .text('United States Educational Attainment');
+
+container
+  .append('p')
+  .attr('id', 'description')
+  .text("Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)")
+
 
